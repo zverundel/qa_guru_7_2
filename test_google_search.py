642316210457e21856d36ead6d2c006d79fbe299
Daniil Zverev <svarhik348@gmail.com>
2023-07-27 00:18:43 +0300

    Внес изменения в проект

--- test_google_search.py
+++ test_google_search.py
@@ -1,12 +1,10 @@
-from selene.support.shared import browser
-from selene import be, have
+from selene import browser, be, have
 
 
 def test_search_result(browser_firefox_open, browser_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
-
 
 
 def test_empty_search_result():
